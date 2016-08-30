8966c3dcad57635274eafbc81ec6a89ec42ef219
nfcf <nunofcf@gmail.com>
2016-08-30 10:06:53 +0100

    removed src android.library and linked to original module

--- com.ivigilate.android.library/src/main/java/com/ivigilate/android/library/classes/ErrorResponse.java
+++
@@ -1,6 +0,0 @@
-package com.ivigilate.android.library.classes;
-
-public class ErrorResponse {
-    public long timestamp;
-    public String data;
-}
--- com.ivigilate.android.library/src/main/java/com/ivigilate/android/library/interfaces/ILocationListener.java
+++
@@ -1,9 +0,0 @@
-package com.ivigilate.android.library.interfaces;
-
-import com.ivigilate.android.library.classes.GPSLocation;
-
-public interface ILocationListener {
-
-    void onLocationChanged(GPSLocation location);
-
-}
